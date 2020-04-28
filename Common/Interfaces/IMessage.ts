a71a5487d0462abf77456e363a8407d25265ab57
ayingkimphillip-official <58986412+ayingkimphillip-official@users.noreply.github.com>
2020-04-28 21:27:46 +0800

    Delete IMessage.ts

--- Common/Interfaces/IMessage.ts
+++
@@ -1,9 +0,0 @@
-import Commands from '../Enums/Commands';
-import MessageTypes from '../Enums/MessageTypes';
-
-export default interface IMessage {
-    command: Commands,
-    nonce: number,
-    status: MessageTypes,
-    payload: string
-}
